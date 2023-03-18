4bc9e6a1505c30e1a765e39a268e19bedb2ad8b4
VibeM4N <123733612+VibeM4N@users.noreply.github.com>
2023-03-18 15:39:02 +0000

    NSFW

---
+++ Commands/NSFW/Pgif.js
@@ -0,0 +1,30 @@
+const NSFW = require("discord-nsfw");
+const { MessageEmbed, Message } = require("discord.js");
+const nsfw = new NSFW();
+
+module.exports = {
+  name: "sgif",
+  category: "NSFW",
+  aliases: ["sex", "p-gif", "pgif"],
+  run: async (client, message, args) => {
+    const image = await nsfw.pgif();
+    const embed = new MessageEmbed()
+      .setTitle(`4K`)
+      .setColor("RANDOM")
+      .setImage(image);
+
+    if (!message.channel.nsfw) {
+      return message.channel.send({
+        embeds: [
+          new MessageEmbed()
+            .setColor("RED")
+            .setDescription(
+              `\`This command contains NSFW content! Use it in NSFW channels!\``
+            ),
+        ],
+      });
+    } else {
+      await message.channel.send({ embeds: [embed] });
+    }
+  },
+};
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Vibe-Bot-v3
- 
